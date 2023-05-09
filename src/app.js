1594c016ff53356f31743397a87c47d933d2d08f
luisangeluis <luisangeluis@gmail.com>
2023-05-08 21:02:47 -0600

    express limit added

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const initModels = require("./models/init.models");
 const defaultData = require("./utils/defaultData");
 const cors = require('cors');
+const apiLimiter = require('./utils/limiter');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(apiLimiter);
 
 //Database
 const { db } = require("./database/database");
@@ -25,14 +27,12 @@ if (process.env.NODE_ENV === 'production') {
     .catch(error => console.log(error))
 } else {
   db.sync({ force: true })
-    // db.sync()
     .then(() => {
       console.log('database synced');
       defaultData();
     })
     .catch(error => console.log(error))
 }
-
 
 //Routes
 const productsRouter = require("./routes/products.routes").router;
